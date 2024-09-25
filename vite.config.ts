dbdf133e18fc51bf161bf84099325dc3ed96b966
Pavlo Boiko <pavlo@boiko.work>
2024-09-25 13:40:41 +0300

    configure paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd())
 
   return {
     root: 'src',
     publicDir: '../public', // relative to 'src'
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     server: {
-      host: env.HOST,
-      port: parseInt(env.PORT, 10),
+      host: env.VITE_HOST,
+      port: parseInt(env.VITE_PORT, 10),
     },
   }
 })
